f615409635cf151d0837c03e076fb3c262ee8796
hangunhee94 <webzen9422@gmail.com>
2022-06-03 17:14:19 +0900

    Add : product app

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.category
+
+    category = models.CharField(max_length=256)
+
+class Drink(models.Model):
+    class Meta:
+        db_table = "drinks"
+
+    def __str__(self):
+        return self.drink_name
+
+    drink_name = models.CharField(max_length=256)
+    category = models.CharField(max_length=256)
+    menu = models.ManyToManyField(Category)
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from . models import Drink, Category
 # Create your views here.
+def choice_view(request):
+    if request.method == 'GET':
+        menu_list = Drink.objects.all()
+        return render(request, 'product/menu_list.html', {'menu_list': menu_list})
